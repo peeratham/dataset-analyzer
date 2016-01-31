e1adfb8ca3203b991704bcdc1332a01445ed2939
peeratham <tpeera4@vt.edu>
2016-01-30 23:12:43 -0500

    refactored Main

--- src/main/java/cs/vt/analysis/analyzer/Main.java
+++ src/main/java/cs/vt/analysis/analyzer/Main.java
@@ -1,90 +1,44 @@
 package cs.vt.analysis.analyzer;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-
-
-
-
-
-
-
-
 import org.json.simple.parser.ParseException;
 
 import cs.vt.analysis.analyzer.analysis.UnreachableCodeAnalyzer;
 import cs.vt.analysis.analyzer.nodes.ScratchProject;
 import cs.vt.analysis.analyzer.parser.ParsingException;
-import cs.vt.analysis.analyzer.visitor.DownUp;
-import cs.vt.analysis.analyzer.visitor.Identity;
-import cs.vt.analysis.analyzer.visitor.Printer;
-import cs.vt.analysis.analyzer.visitor.Stop;
-import cs.vt.analysis.analyzer.visitor.VisitFailure;
-import cs.vt.analysis.analyzer.visitor.Visitor;
 
 public class Main {
+	static Logger logger = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-		
-
         // Configure Log4J
-        PropertyConfigurator
-          .configure(Main.class.getClassLoader()
-                      .getResource("log4j.properties"));
+        PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
         
-        JSONParser jsonParser = new JSONParser();
-        ScratchProject project = null;
-        
-//        try {
-//
-//        	InputStream in = Main.class.getClassLoader().getResource("project03.json").openStream();
-//        	
-//			Object obj = jsonParser.parse((new BufferedReader(new InputStreamReader(in))));
-//            JSONObject jsonObject = (JSONObject) obj;
-//            project = ScratchProject.loadProject(jsonObject);
-//
-//  
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        UnreachableCodeAnalyzer analyzer = new UnreachableCodeAnalyzer(project);
-//		analyzer.analyze();
-//		System.out.println(analyzer.getReport().getSummary());
-//		System.out.println(analyzer.getReport().getFullReport());
-		
-		
-		String pathToDataset = "C:\\Users\\Peeratham\\workspace\\scratch-dataset";
-		File datasetDirectory = new File(pathToDataset);
+        String pathToDataset = "C:\\Users\\Peeratham\\workspace\\scratch-dataset";
+        File datasetDirectory = new File(pathToDataset);
+     
 		File[] files = datasetDirectory.listFiles();
 		for (int i = 0; i < files.length; i++) {
 			try {
-				System.out.println(files[i]);
-				String string = FileUtils.readFileToString(files[i]);
-				try {
-					project = ScratchProject.loadProject(string);
-				} catch (ParsingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				ScratchProject project = ScratchProject.loadProject(FileUtils.readFileToString(files[i]));
 				UnreachableCodeAnalyzer analyzer = new UnreachableCodeAnalyzer(project);
 				analyzer.analyze();
-				System.out.println(analyzer.getReport().getSummary());
-				System.out.println(analyzer.getReport().getFullReport());
-				
+				logger.info(analyzer.getReport().getSummary());
+				logger.info(analyzer.getReport().getFullReport());
 			} catch (IOException e) {
-//				e.printStackTrace();
+				logger.error("Fail to read file:"+e.getMessage());
 			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-//				e.printStackTrace();
+				logger.error("Fail to parse JSON file:"+files[i]);
+				e.printStackTrace();
+			} catch (ParsingException e) {
+				logger.error("Fail to load project:"+files[i]);
+				logger.error(e.getMessage());
+				e.printStackTrace();
 			}
 			
 		}
