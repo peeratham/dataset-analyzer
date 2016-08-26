67bf804ee3e1e778d1defe4fabd3917aa0327fc5
Peeratham Techapalokul <tpeera4@vt.edu>
2016-08-26 11:04:38 -0400

    fix bug

--- src/main/java/vt/cs/smells/analyzer/analysis/UnusedCustomBlockAnalyzer.java
+++ src/main/java/vt/cs/smells/analyzer/analysis/UnusedCustomBlockAnalyzer.java
@@ -2,8 +2,10 @@ package vt.cs.smells.analyzer.analysis;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
@@ -14,6 +16,8 @@ import vt.cs.smells.analyzer.Analyzer;
 import vt.cs.smells.analyzer.ListAnalysisReport;
 import vt.cs.smells.analyzer.Report;
 import vt.cs.smells.analyzer.nodes.Block;
+import vt.cs.smells.analyzer.nodes.BlockType;
+import vt.cs.smells.analyzer.nodes.CustomBlock;
 import vt.cs.smells.analyzer.nodes.ScratchProject;
 import vt.cs.smells.analyzer.nodes.Scriptable;
 import vt.cs.smells.analyzer.parser.ParsingException;
@@ -21,67 +25,72 @@ import vt.cs.smells.analyzer.parser.Util;
 import vt.cs.smells.select.Collector;
 import vt.cs.smells.select.Evaluator;
 
-public class UnusedCustomBlockAnalyzer extends Analyzer{
+public class UnusedCustomBlockAnalyzer extends Analyzer {
 	private static final String name = "UnusedCustomBlock";
 	private static final String abbr = "UCB";
-	
-	
+
 	public HashSet<String> allBlock = new HashSet<String>();
 	List<String> blockRelatedCommands = new ArrayList<String>();
-	private ListAnalysisReport report = new ListAnalysisReport(name,abbr);
-	public  ArrayList<Block> allBlocks;
+	private ListAnalysisReport report = new ListAnalysisReport(name, abbr);
+	public ArrayList<Block> allBlocks;
 	int count = 0;
-	private List<Block> allCustomBlocks;
-	
-	public UnusedCustomBlockAnalyzer(){
+	boolean haveCustomBlock = false;
+
+	Map<Scriptable, List<String>> scriptableToCustomBlocks = new HashMap<>();
+
+	public UnusedCustomBlockAnalyzer() {
 		blockRelatedCommands.add("call");
 	}
 
-	public void analyze() throws AnalysisException{
-		allCustomBlocks = Collector.collect(new Evaluator.BlockCommand("procDef"), project);
+	public void analyze() throws AnalysisException {
 		
-		Scriptable stage = project.getScriptable("Stage");		
-		for (String name : project.getAllScriptables().keySet()) {
-			if(name!="Stage"){
-				allBlocks = AnalysisUtil.findBlock(project.getScriptable(name), "procDef"); 
-			}
-		}
-		try {
-			if(allBlocks!=null){
-				for(Block block :allBlocks){
-					allBlock.add(block.getArgs(0).toString());
-				}
-			}
-			for(String blockCommand : blockRelatedCommands){
-				ArrayList<Block> varBlocks = Collector.collect(new Evaluator.BlockCommand(blockCommand), project);			
-				for (Block block : varBlocks) {				
-					String temp = block.getBlockType().getSpec().toString();
-					if(allBlock.contains(temp)){
-						allBlock.remove(temp);
-					}
-				}
-			}		
-		} catch (Exception e) {
-			throw new AnalysisException(e);
-		}
 		
-		for (String s:allBlock) {
-			report.addRecord(s);
-			count++;
+		for (String scriptable : project.getAllScriptables().keySet()) {
+			List<Block> thisScriptableCustomBlocks = Collector.collect(new Evaluator.BlockCommand(
+					"procDef"), project.getScriptable(scriptable));
+			
+			HashSet<String> definedCustomBlocks = new HashSet<String>();
+			for (Block customBlock : thisScriptableCustomBlocks) {
+				CustomBlock cb = (CustomBlock) customBlock.getArgs(0);
+				definedCustomBlocks.add(cb.getBlockType().getSpec().toString());
+				
+			}
+			
+			HashSet<String> calledCustomBlockNames = new HashSet<String>();
+			ArrayList<Block> calledCustomBlock = Collector.collect(
+					new Evaluator.BlockCommand("call"), project.getScriptable(scriptable));
+			
+			
+			for (Block customBlock : calledCustomBlock) {
+				calledCustomBlockNames.add(customBlock.getBlockType().getSpec().toString());
+			}
+			
+			//check if customblock is used
+			haveCustomBlock = haveCustomBlock||!definedCustomBlocks.isEmpty();
+			
+			definedCustomBlocks.removeAll(calledCustomBlockNames);
+			if(!definedCustomBlocks.isEmpty()){
+				for(String blockName:definedCustomBlocks){
+					report.addRecord(scriptable+"["+blockName+"]");
+					count++;
+				}	
+			}
 		}
 	}
-	
+
 	public Report getReport() {
 		JSONObject conciseReport = new JSONObject();
-		if(!allCustomBlocks.isEmpty()){
+		if(haveCustomBlock){
 			conciseReport.put("count", count);
 		}
 		report.setConciseJSONReport(conciseReport);
+
 		return report;
 	}
-	
-	public static void main(String[] args) throws IOException, ParseException, ParsingException, AnalysisException{
-		String projectSrc = Util.retrieveProjectOnline(118377854);
+
+	public static void main(String[] args) throws IOException, ParseException,
+			ParsingException, AnalysisException {
+		String projectSrc = Util.retrieveProjectOnline(119170531);
 		ScratchProject project = ScratchProject.loadProject(projectSrc);
 		UnusedCustomBlockAnalyzer analyzer = new UnusedCustomBlockAnalyzer();
 		analyzer.setProject(project);
@@ -89,5 +98,5 @@ public class UnusedCustomBlockAnalyzer extends Analyzer{
 		System.out.println(analyzer.getReport().getJSONReport());
 		System.out.println(analyzer.getReport().getConciseJSONReport());
 	}
-	
+
 }
